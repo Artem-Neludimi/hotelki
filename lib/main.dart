ea7ef1f29f773cf712c98a460f0bde508d5d4797
artem <art.zavtur@gmail.com>
2023-10-30 21:45:54 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:loggy/loggy.dart';
 import 'package:scalable_flutter_app_starter/core/app/app.dart';
+import 'package:scalable_flutter_app_starter/firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   _initLoggy();
   _initGoogleFonts();
 
