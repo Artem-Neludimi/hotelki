615ff659e945661b888359dc51886d7ba3804c5f
artem <art.zavtur@gmail.com>
2023-11-04 21:50:12 +0200

    settings screen refactoring

--- lib/feature/settings/ui/page/settings_page.dart
+++ lib/feature/settings/ui/page/settings_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:scalable_flutter_app_starter/core/ui/dialog/dialogs.dart';
-import 'package:scalable_flutter_app_starter/core/util/urls.dart';
-import 'package:scalable_flutter_app_starter/feature/settings/ui/widget/app_version.dart';
 import 'package:scalable_flutter_app_starter/feature/settings/ui/widget/settings_tile.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -16,16 +14,6 @@ class SettingsPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          const SettingsTile(
-            icon: Icons.security_outlined,
-            label: 'Privacy Policy',
-            onTap: Urls.showPrivacyPolicy,
-          ),
-          const SettingsTile(
-            icon: Icons.fact_check_outlined,
-            label: 'Terms of Service',
-            onTap: Urls.showTermsOfService,
-          ),
           SettingsTile(
             icon: Icons.delete_outlined,
             label: 'Delete Account',
@@ -35,11 +23,6 @@ class SettingsPage extends StatelessWidget {
             icon: Icons.logout_outlined,
             label: 'Sign Out',
             onTap: () => Dialogs.showLogOutConfirmationDialog(context),
-          ),
-          const Spacer(),
-          const Padding(
-            padding: EdgeInsets.all(16),
-            child: AppVersion(),
           ),
         ],
       ),
